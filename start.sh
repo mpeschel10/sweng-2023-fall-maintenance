193a4a349a83eb59e71978d48bb1f9a78b715142
Mark Peschel <mpeschel10@gmail.com>
2023-11-15 00:25:56 -0500

    Startup script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+systemctl is-active --quiet nginx || sudo systemctl start nginx
+systemctl is-active --quiet mariadb || sudo systemctl start mariadb
+mariadb --user=AzureDiamond --password=hunter2 -D sweng < maintenance.sql
+
+cargo run & bg
+gio open "http://localhost"
+fg
