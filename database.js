2f1711e6c5a23b2345c39932ee06209ad7e1ed21
Mark Peschel <mpeschel10@gmail.com>
2023-12-03 18:18:04 -0500

    Asynchronous, modular wrapper database.js

--- database.js
+++ database.js
@@ -1,4 +1,18 @@
-const { PI } = Math;
+const mysql = require('mysql');
 
-exports.area = radius => PI * radius * radius;
+exports.connection = () => mysql.createConnection({
+	host     : 'localhost',
+	user     : 'AzureDiamond',
+	password : 'hunter2',
+	database : 'sweng'
+});
 
+exports.query = (connection, string) => new Promise((resolve, reject) => {
+	connection.query('SELECT * FROM users', function (error, results, fields) {
+		if (error) {
+			reject(error);
+		} else {
+			resolve(results, fields);
+		}
+	});
+});
--- test.js
+++ test.js
@@ -1,31 +1,15 @@
-const database = require('./database.js')
+const database = require('./database.js');
 
-console.log(database.area(1));
+const connection = database.connection();
+async function main() {
+	console.log("Start of main function");
+	try {
+		const response = await database.query(connection, "Select * from requests");
+		console.log("The winner is: ", response);
+	} catch (e) {
+		console.error("Error!", e);
+	}
+	process.exit();
+}
 
-// // From https://www.npmjs.com/package/mysql
-// const mysql      = require('mysql');
-// const connection = mysql.createConnection({
-// 	host     : 'localhost',
-// 	user     : 'AzureDiamond',
-// 	password : 'hunter2',
-// 	database : 'sweng'
-// });
- 
-// connection.connect();
- 
-// // results = [row]
-// // row = {attribute:value}
-// connection.query('SELECT * FROM users', function (error, results, fields) {
-// 	if (error) throw error;
-// 	console.log('The solution is: ', results);
-// });
-
-
-// connection.query('SELECT * FROM requests', function (error, results, fields) {
-// 	if (error) throw error;
-// 	console.log('The solution is: ', results);
-// });
-  
-// connection.end();
-   
-	
+main();
