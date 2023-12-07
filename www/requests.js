3c361bde2c8abe303807ef87e4fbf2e8b302a943
Mark Peschel <mpeschel10@gmail.com>
2023-12-06 20:25:57 -0500

    Show list of requests in requests.html Redo table as grid. Call search when enter button pressed. Submit params though that isn't implemented server-side yet.

---
+++ www/requests.js
@@ -0,0 +1,46 @@
+(() => {
+	async function search() {
+		const formSearch = document.getElementById("form-search");
+		const params = new URLSearchParams(new FormData(formSearch));
+		
+		const requests = await fetch("/request?" + params).then(response => response.json());
+		
+		const oldHeader = document.querySelectorAll(".th");
+		const table = document.getElementById("table-requests");
+		table.replaceChildren(...oldHeader);
+
+		const n = s => document.importNode(templateRow.content.querySelector(s), true);
+		const templateRow = document.getElementById("template-row");
+		for (const request of requests) {
+			const apartment = n(".span-apartment");
+			const location = n(".span-location");
+			const date = n(".span-date");
+			const status = n(".span-status");
+			const view = n(".a-view");
+			
+			apartment.textContent = request.apartment;
+			location.textContent = request.location;
+			date.textContent = new Date(parseInt(request.datetime) * 1000);
+			status.textContent = request.status;
+			view.href = `/request.html?id=${request.id}`;
+			
+			table.appendChild(apartment);
+			table.appendChild(location);
+			table.appendChild(date);
+			table.appendChild(status);
+			table.appendChild(view);
+		}
+	}
+
+	function onButtonSubmit(event) {
+		event.preventDefault();
+		search();
+	}
+
+	function init() {
+		document.getElementById("button-submit").addEventListener("click", onButtonSubmit);
+		search();
+	}
+
+	window.addEventListener("load", init);
+})();
